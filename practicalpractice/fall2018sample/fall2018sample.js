08f6b743fbe3cda76c5c1d0c63d8a7f94e054131
celeste1337 <10539434+celeste1337@users.noreply.github.com>
2018-12-06 16:21:52 -0500

    practice practical (lol) done

--- practicalpractice/fall2018sample/fall2018sample.js
+++ practicalpractice/fall2018sample/fall2018sample.js
@@ -1,1 +1,19 @@
 /* Don't use <script> tags in a linked js file! */
+'use strict';
+let file = ("content1.txt");
+
+$( document ).ready(function() { //hide onload
+    $(".submenu").hide();
+});
+
+$(".menuitem").click(function () { 
+    $(this).next(".submenu").slideToggle("slow");
+});
+
+$("#content-menu").val(file);
+$("#content").load(file);
+
+$("#choose-content").change(function () {
+    file = $(this).val();
+    $("#content").load(file);
+});
