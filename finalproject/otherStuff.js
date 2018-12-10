221ab121d915aec930c542b86fb6332685bd0783
Celeste Yost <10539434+celeste1337@users.noreply.github.com>
2018-12-10 04:20:57 -0500

    docs time

--- finalproject/otherStuff.js
+++ finalproject/otherStuff.js
@@ -1,7 +1,24 @@
+
+
 $("#secondGraphic img").mouseover(function() {
     $("circle").css({fill: "darkred"});
 });
 
 $("#secondGraphic img").mouseleave(function() {
     $("circle").css({fill: "white"});
+});
+
+let lightsOn = true;
+$("#thirdGraphic .button").click(function() {
+    if(lightsOn) {
+        $("#thirdGraphic img").css({filter: "brightness(30%)"});
+        $("#thirdGraphic object").css({filter: "brightness(30%)"});
+        lightsOn = false;
+    }
+    else {
+        $("#thirdGraphic img").css({filter: "brightness(100%)"});
+        $("#thirdGraphic object").css({filter: "brightness(100%)"});
+        lightsOn = true;
+    }
+
 });
