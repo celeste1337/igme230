1c53a64dbc5df90f061bd3e60e52e5721c03c644
Celeste Yost <10539434+celeste1337@users.noreply.github.com>
2018-12-09 19:49:26 -0500

    bg imgs to have transparent canvas over them

--- finalproject/pixiStuff.js
+++ finalproject/pixiStuff.js
@@ -5,10 +5,14 @@
 //$( "#navButtons" ).find( "li" ).eq().addClass( "active" );
 
 //first canvas
-let app1 = new PIXI.Application({width: 256, height: 256});
+let app1 = new PIXI.Application({width: 800, height: 600});
+
 
 //second canvas
-let app2 = new PIXI.Application({width: 256, height: 256});
+let app2 = new PIXI.Application({width: 800, height: 600});
+
+
+
 
 //third canvas
-let app3 = new PIXI.Application({width: 256, height: 256});
+let app3 = new PIXI.Application({width: 800, height: 600});
