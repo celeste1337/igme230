badc750ea1e6659d8b63a7d04c38e8478b955e10
Celeste Yost <10539434+celeste1337@users.noreply.github.com>
2018-12-09 19:25:00 -0500

    starting pixijs

---
+++ finalproject/pixiStuff.js
@@ -0,0 +1,14 @@
+//onload
+
+
+//assign class to current position
+//$( "#navButtons" ).find( "li" ).eq().addClass( "active" );
+
+//first canvas
+let app1 = new PIXI.Application({width: 256, height: 256});
+
+//second canvas
+let app2 = new PIXI.Application({width: 256, height: 256});
+
+//third canvas
+let app3 = new PIXI.Application({width: 256, height: 256});
