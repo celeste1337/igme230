878d10ef415a3510cf5b5507d2d4108b0080ecf7
Celeste Yost <10539434+celeste1337@users.noreply.github.com>
2018-12-10 03:14:11 -0500

    second sorta done

---
+++ finalproject/otherStuff.js
@@ -0,0 +1,7 @@
+$("#secondGraphic img").mouseover(function() {
+    $("circle").css({fill: "darkred"});
+});
+
+$("#secondGraphic img").mouseleave(function() {
+    $("circle").css({fill: "white"});
+});
