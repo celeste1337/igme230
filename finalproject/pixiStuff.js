31d95eba6da923aad30d4ca5eede17168ae96bcb
Celeste Yost <10539434+celeste1337@users.noreply.github.com>
2018-12-09 20:43:28 -0500

    mug added to canvas

--- finalproject/pixiStuff.js
+++ finalproject/pixiStuff.js
@@ -4,15 +4,39 @@
 //assign class to current position
 //$( "#navButtons" ).find( "li" ).eq().addClass( "active" );
 
+let app1Coords = $("#firstGraphic img").offset();
+console.log(app1Coords.left);
+
+//Aliases
+let Application = PIXI.Application,
+    loader = PIXI.loader,
+    resources = PIXI.loader.resources,
+    Sprite = PIXI.Sprite;
+
 //first canvas
-let app1 = new PIXI.Application({width: 800, height: 600});
+let app1 = new PIXI.Application({ 
+    width: 800,         // default: 800
+    height: 600,        // default: 600
+    antialias: true,    // default: false
+    transparent: true, // default: false
+    resolution: 1       // default: 1
+  }
+);
+document.body.appendChild(app1.view);
+$("canvas").offset(app1Coords); //scoots the canvas element to the position of the first image
 
+//load an image and run the `setup` function when it's done
+loader
+  .add("media/coffeemug.png")
+  .load(setup);
 
-//second canvas
-let app2 = new PIXI.Application({width: 800, height: 600});
+//This `setup` function will run when the image has loaded
+function setup() {
 
-
-
-
-//third canvas
-let app3 = new PIXI.Application({width: 800, height: 600});
+  //Create the cat sprite
+  let mug = new Sprite(resources["media/coffeemug.png"].texture);
+  mug.x = 550;
+  mug.y = 340;
+  //Add the cat to the stage
+  app1.stage.addChild(mug);
+}
